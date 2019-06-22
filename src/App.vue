2b2be93fab82cf9410b992f4c437cc71c9b9e418
lukasborawski <lukas.borawski@gmail.com>
2019-06-22 19:39:11 +0200

    chore: Stage-1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="css">
+  body {
+      width: 100%;
+      height: 100%;
+  }
+</style>
